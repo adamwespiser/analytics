7bcac3da5024020b31e7a41f8b5c07726db7403f
Adam Wespiser <adamwespiser@gmail.com>
2019-09-28 00:39:50 -0400

    Add Authorization to API/API_KEY

--- app/Main.hs
+++ app/Main.hs
@@ -1,87 +1,69 @@
 module Main where
 
-import           GHC.Generics (Generic)
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import qualified Data.Text as T
-import           Data.Aeson (ToJSON, FromJSON)
--- import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import           System.Environment
-import           System.IO (hPutStrLn, stderr)
-import           Servant
-import           Database.Beam as B
-import qualified Database.Beam.Query as BeamQ
-import qualified Database.Beam.Postgres as Pg
-import qualified Data.ByteString.Char8 as BSC
+import           Control.Monad.Extra         (ifM)
 import           Control.Monad.IO.Class
-import           Data.Maybe (fromMaybe)
-import           Text.Read (readMaybe)
-import           Control.Monad.Trans.Reader  (ReaderT, ask, runReaderT)
+import           Control.Monad.Trans.Class   (lift)
+import           Control.Monad.Trans.Except  (ExceptT, throwE)
+import           Control.Monad.Trans.Reader  (ReaderT, ask, asks, runReaderT)
 
+import           Data.Aeson                  (ToJSON, FromJSON)
+import qualified Data.ByteString.Char8       as BSC
+
+import           Database.Beam               as B
+import qualified Database.Beam.Query         as BeamQ
+import qualified Database.Beam.Postgres      as Pg
+import           Data.Maybe                  (fromMaybe)
+
+import qualified Data.Text      as T
+import           GHC.Generics   (Generic)
+import Network.Wai.Handler.Warp (setPort, setBeforeMainLoop, defaultSettings, runSettings)
+
+import           Servant
+import           Servant.Server     (ServerError(..), err403)
+
+import           System.Environment (getEnv)
+import           System.IO          (hPutStrLn, stderr)
+import           Text.Read          (readMaybe)
+
+
+---------------------------------------------------------
 import Lib
 import Db
-
-
-data Event = Event {
-  evSessionTrackingId :: T.Text,
-  evCategory :: T.Text,
-  evLabel :: T.Text
-} deriving (Eq, Show, Generic)
-instance ToJSON Event
-instance FromJSON Event
-
-data PageView = PageView {
-  pgSessionTrackingID :: T.Text,
-  pgUrlFilePath :: T.Text
-} deriving (Eq, Show, Generic)
-instance ToJSON PageView
-instance FromJSON PageView
-
-class ToDatabase a b where
-  convertToDb :: a -> b
-
-instance ToDatabase PageView (PageViewDBT (QExpr Pg.Postgres s)) where
-  convertToDb pageview@PageView{..} =
-    PageViewDB
-      B.default_
-      (BeamQ.val_ pgSessionTrackingID)
-      (BeamQ.val_ pgUrlFilePath)
-      Pg.now_
-
-instance ToDatabase Event (EventsDBT (QExpr Pg.Postgres s)) where
-  convertToDb event@Event{..} =
-    EventsDB
-      B.default_
-      (BeamQ.val_ evSessionTrackingId)
-      (BeamQ.val_ evCategory)
-      (BeamQ.val_ evLabel)
-      Pg.now_
+import ApiTypes (PageView(..), Event(..), ToDatabase, convertToDb)
 
 
 type PostAPI =
-  "event" :> ReqBody '[JSON] Event    :> Post '[JSON] Int :<|>
-  "page"  :> ReqBody '[JSON] PageView :> Post '[JSON] Int
+  Header "Authorization" T.Text :> "event" :> ReqBody '[JSON] Event    :> Post '[JSON] Int :<|>
+  Header "Authorization" T.Text :> "page"  :> ReqBody '[JSON] PageView :> Post '[JSON] Int
+
+withAuth :: Maybe T.Text -> AppM a -> AppM a
+withAuth auth f =
+  ifM (isCorrectAuth auth)
+      (f)
+      (lift $ Handler $ throwE err403)
 
 server :: ServerT PostAPI AppM
 server =
   postEvent :<|>
   postPageView
   where
-    postEvent :: Event -> AppM Int
-    postEvent event@Event{..} = do
-      Ctx{..} <- ask
-      liftIO $ print event
-      status <- liftIO $ Pg.runBeamPostgresDebug putStrLn conn $ runInsert $
-        insert (dbEvents analyticsDb) $ insertExpressions [convertToDb event]
-      liftIO $ print status
-      return 1
-    postPageView :: PageView -> AppM Int
-    postPageView pageview@PageView{..} = do
-      Ctx{..} <- ask
-      liftIO $ print pageview
-      status  <- liftIO $ Pg.runBeamPostgresDebug putStrLn conn $ runInsert $
-        insert (dbPageView analyticsDb) $ insertExpressions [convertToDb pageview]
-      return 1
+    postEvent :: Maybe T.Text -> Event -> AppM Int
+    postEvent auth event@Event{..} =
+      withAuth auth $ do
+        Ctx{..} <- ask
+        liftIO $ print event
+        status <- liftIO $ Pg.runBeamPostgresDebug putStrLn conn $ runInsert $
+          insert (dbEvents analyticsDb) $ insertExpressions [convertToDb event]
+        liftIO $ print status
+        return 1
+    postPageView :: Maybe T.Text -> PageView -> AppM Int
+    postPageView auth pageview@PageView{..} =
+      withAuth auth $ do
+        Ctx{..} <- ask
+        liftIO $ print pageview
+        status  <- liftIO $ Pg.runBeamPostgresDebug putStrLn conn $ runInsert $
+          insert (dbPageView analyticsDb) $ insertExpressions [convertToDb pageview]
+        return 1
 
 
 api :: Proxy PostAPI
@@ -91,19 +73,29 @@ app :: Ctx -> Application
 app s = serve api $ hoistServer api (`runReaderT` s) server
 
 data Ctx = Ctx {
-  port :: Int,
-  conn :: Pg.Connection
+  conn :: Pg.Connection,
+  apiKey :: T.Text
 }
 
 type AppM = ReaderT Ctx Handler
+--             newtype Handler a = Handler { runHandler' :: ExceptT ServerError IO a }
+
+
+class (Monad m) => MonadAuth m where
+  isCorrectAuth :: Maybe T.Text -> m Bool
+
+instance (Monad m) => MonadAuth (ReaderT Ctx m) where
+  isCorrectAuth auth = do
+    key <- asks apiKey
+    pure $ auth == Just key
 
 main :: IO ()
 main = do
   port <- fromMaybe (error "Env var PORT must be set") . readMaybe <$> getEnv "PORT"
-  connectionStr <- BSC.pack <$> getEnv "DBCONN"
-  conn <- Pg.connectPostgreSQL connectionStr
+  ctx <- Ctx <$>
+          (BSC.pack <$> getEnv "DBCONN" >>= Pg.connectPostgreSQL) <*>
+          (T.pack <$> getEnv "API_KEY")
   let settings = setPort port $
         setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
         defaultSettings
-  let ctx = Ctx port conn
   runSettings settings (app ctx)
--- test/curl-event.sh
+++ test/curl-event.sh
@@ -3,6 +3,7 @@ echo "localhost/page"
 curl \
   -X POST \
   -H "content-type: application/json" \
+  -H "Authorization: ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789" \
   -d '{"pgSessionTrackingID":"test1", "pgUrlFilePath": "test23"}' \
   localhost:8080/page
 echo ""
@@ -11,6 +12,7 @@ echo "localhost/event"
 curl \
   -X POST \
   -H "content-type: application/json" \
+  -H "Authorization: ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789" \
   -d '{"evSessionTrackingId":"test1", "evCategory": "cat1", "evLabel":"label1"}' \
   localhost:8080/event
 echo ""
